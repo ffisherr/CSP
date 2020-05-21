8740187c17a2b5f5123d90fbd6cd2fb197140473
Andrew <andreipopov1998@mail.ru>
2020-05-21 14:33:28 +0300

    Add commit

---
+++ CSP/app/src/main/java/com/ffisherr/csp/Enter.java
@@ -0,0 +1,15 @@
+package com.ffisherr.csp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class Enter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acnivity_enter);
+    }
+}
---
+++ CSP/app/src/main/java/com/ffisherr/csp/MainActivity.java
@@ -0,0 +1,44 @@
+package com.ffisherr.csp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button In = (Button)findViewById(R.id.AutoButton);
+        In.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(MainActivity.this,Enter.class);
+                    startActivity(intent);finish();
+                }catch (Exception e){
+
+                }
+            }
+        });
+
+        Button Reg = (Button)findViewById(R.id.RegistrButton);
+        Reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent1 = new Intent(MainActivity.this, Registr.class);
+                    startActivity(intent1);finish();
+                }catch (Exception e){
+
+                }
+            }
+        });
+
+    }
+}
