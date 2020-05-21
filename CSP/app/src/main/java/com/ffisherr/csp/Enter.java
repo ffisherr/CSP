60722a6966fd89b3f3225ffba835d4aac6c10b44
Pukish <pda0610@yandex.ru>
2020-05-21 17:57:32 +0300

    Экран заявки которую видит тех. поддержка

--- CSP/app/src/main/java/com/ffisherr/csp/Enter.java
+++ CSP/app/src/main/java/com/ffisherr/csp/Enter.java
@@ -1,6 +1,11 @@
 package com.ffisherr.csp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -11,5 +16,20 @@ public class Enter extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acnivity_enter);
+
+        Button Reg = (Button)findViewById(R.id.Enter_Button);
+        Reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent1 = new Intent(Enter.this, TechUserActivity.class);
+                    startActivity(intent1);finish();
+                }catch (Exception e){
+                    Log.e("Start register activity", e.toString());
+                }
+            }
+        });
+
+
     }
 }
