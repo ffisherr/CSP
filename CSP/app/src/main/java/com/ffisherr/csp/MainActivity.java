416ce89f408ddde8dfcf684ab1d34afe64e4dd2b
Andrew <andreipopov1998@mail.ru>
2020-05-21 15:44:25 +0300

    Bug fix

--- CSP/app/src/main/java/com/ffisherr/csp/MainActivity.java
+++ CSP/app/src/main/java/com/ffisherr/csp/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,15 +16,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button In = (Button)findViewById(R.id.AutoButton);
+        Button In = findViewById(R.id.AutoButton);
         In.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    Intent intent = new Intent(MainActivity.this,Enter.class);
+                    Intent intent = new Intent(MainActivity.this, Enter.class);
                     startActivity(intent);finish();
                 }catch (Exception e){
-
+                    Log.e("Start enter activity", e.toString());
                 }
             }
         });
@@ -35,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
                     Intent intent1 = new Intent(MainActivity.this, Registr.class);
                     startActivity(intent1);finish();
                 }catch (Exception e){
-
+                    Log.e("Start register activity", e.toString());
                 }
             }
         });
