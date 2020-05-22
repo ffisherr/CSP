f0c99aaad98e05e04e27d34be46b593824a0dc6a
Andrew <andreipopov1998@mail.ru>
2020-05-22 11:48:59 +0300

    Show tech

--- CSP/app/src/main/java/com/ffisherr/csp/BosTech.java
+++ CSP/app/src/main/java/com/ffisherr/csp/BosTech.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.ffisherr.csp.retrofit.ElementsFounded;
+import com.ffisherr.csp.retrofit.controllers.FindJobForTechController;
 import com.ffisherr.csp.retrofit.controllers.FindTechController;
 import com.ffisherr.csp.users.User;
 
@@ -18,6 +21,7 @@ public class BosTech extends AppCompatActivity implements ElementsFounded {
 
     ArrayAdapter<String> adapter;
     List<String> userNames;
+    List<Integer> userIds;
     ListView listView;
 
     @Override
@@ -26,6 +30,10 @@ public class BosTech extends AppCompatActivity implements ElementsFounded {
         setContentView(R.layout.activity_bos_tech);
         Log.e("", "Зашли куда нужно");
         listView = findViewById(R.id.list_tech_users);
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            FindJobForTechController controller = new FindJobForTechController();
+            controller.start(this, userIds.get(position));
+        });
         FindTechController controller = new FindTechController();
         controller.start(this);
     }
@@ -34,12 +42,16 @@ public class BosTech extends AppCompatActivity implements ElementsFounded {
     public void TechUsersFounded(List<User> users) {
         Log.e("", "Данные получены");
         List<String> userNamesLocal = new ArrayList<>();
+        userIds = new ArrayList<>();
         for (User u : users) {
             userNamesLocal.add(u.getSurName() + " " + u.getFirstName());
+            userIds.add(u.getId());
             System.out.println(u.getSurName() + " " + u.getFirstName());
         }
         userNames = userNamesLocal;
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, userNamesLocal);
         listView.setAdapter(adapter);
     }
+
+
 }
--- CSP/app/src/main/java/com/ffisherr/csp/Registr.java
+++ CSP/app/src/main/java/com/ffisherr/csp/Registr.java
@@ -1,15 +1,9 @@
 package com.ffisherr.csp;
 
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,11 +11,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.ffisherr.csp.retrofit.body.RegisterBody;
 import com.ffisherr.csp.retrofit.controllers.RegisterUserController;
 import com.ffisherr.csp.users.User;
-import com.google.gson.Gson;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.concurrent.ExecutionException;
 
 public class Registr extends AppCompatActivity {
 
@@ -32,92 +24,24 @@ public class Registr extends AppCompatActivity {
     }
 
     public void onClickRegister (View view) {
-        RegisterBody User = new RegisterBody();
+        RegisterBody user = new RegisterBody();
 
-        EditText editText = findViewById(R.id.edit_password_reg);
-        EditText repeatPassword = findViewById(R.id.edit_repeatPassword);
+        EditText editText;
 
-        TextView infText = findViewById(R.id.info);
+        editText = findViewById(R.id.edit_SecondName);
+        user.surName = (editText.getText().toString());
 
-        String p1 = editText.getText().toString();
-        String p2 = repeatPassword.getText().toString();
+        editText = findViewById(R.id.edit_FirstName);
+        user.firstName = (editText.getText().toString());
 
-        if (p1.equals(p2)) {
+        editText = findViewById(R.id.edit_surname);
+        user.secondName = (editText.getText().toString());
 
-            editText = findViewById(R.id.edit_FirstName);
-            User.firstName(editText.getText().toString());
+        editText = findViewById(R.id.edit_login);
+        user.email = (editText.getText().toString());
 
-            editText = findViewById(R.id.edit_surname);
-            User.surName(editText.getText().toString());
-
-            editText = findViewById(R.id.edit_SecondName);
-            User.secondName(editText.getText().toString());
-
-            User.setPassw_hash(p1);
-            User.setRole_id(1);
-
-
-
-            editText = findViewById(R.id.edit_login);
-            User.setLogin(editText.getText().toString());
-
-            if (!User.checkIfOk()) {
-                Toast.makeText(this, "Не заполнены обязательные поля", Toast.LENGTH_LONG).show();
-                return;
-            }
-
-            String result;
-            TaskPostServer ts = new TaskPostServer();
-            String url = ServerDescriptor.serverIpAdress + "/user_add";
-
-            Gson g = new Gson();
-            String user_data = g.toJson(User);
-            ts.execute(url, user_data);
-
-            try {
-                result = ts.get();
-                if (result.equals("[{'status':'connectionError'}]")){
-                    Toast.makeText(this, "Нет интернет соединения", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                UserResponse ur = g.fromJson(result, UserResponse.class);
-                System.out.println(result);
-                if (ur.getStatus().equals(ServerDescriptor.SUCCESS)) {
-                    infText.setText("");
-
-                    Toast.makeText(this, "Вы зарегестрированы", Toast.LENGTH_LONG).show();
-
-                    Intent mStartActivity = new Intent(Registr.this, MainActivity.class);
-                    int mPendingIntentId = 123456;
-                    PendingIntent mPendingIntent = PendingIntent.getActivity(Registr.this, mPendingIntentId, mStartActivity,
-                            PendingIntent.FLAG_CANCEL_CURRENT);
-                    AlarmManager mgr = (AlarmManager) Registr.this.getSystemService(Context.ALARM_SERVICE);
-                    mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 100, mPendingIntent);
-                    System.exit(0);
-
-                } else if (ur.getStatus().equals(ServerDescriptor.LOGIN_ALREADY_EXISTS_ERROR)){
-                    Toast.makeText(this, "Введенный логин уже используется", Toast.LENGTH_LONG).show();
-                    //infText.setText("Введенный логин уже используется");
-                } else if (ur.getStatus().equals(ServerDescriptor.INTERNET_ERROR)){
-                    Toast.makeText(this, "Не удается получить доступ к серверу", Toast.LENGTH_LONG).show();
-                    //infText.setText("Не удается получить доступ к серверу");
-                } else {
-                    Toast.makeText(this, "Не удается получить доступ к серверу", Toast.LENGTH_LONG).show();
-                    //infText.setText("Не удается получить доступ к серверу");
-                }
-            } catch (InterruptedException e) {
-                infText.setText("error");
-                e.printStackTrace();
-            } catch (ExecutionException e) {
-                infText.setText("error");
-                e.printStackTrace();
-            }
-
-        } else {
-            Toast.makeText(this, "Пароли не совпадают", Toast.LENGTH_LONG).show();
-        }
-    }
-
+        editText = findViewById(R.id.edit_password_reg);
+        user.password = (editText.getText().toString());
 
         /*SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
         try {
@@ -125,13 +49,19 @@ public class Registr extends AppCompatActivity {
             user.setBirthDate(formatter.parse(editText.getText().toString()));
 
             editText = findViewById(R.id.ContracDate_text);
-            user.setEndOfContract(formatter.parse(editText.getText().toString()));
+            user. = (formatter.parse(editText.getText().toString()));
         } catch (ParseException e) {
-            // TODO Высплывающее окно при неправильном вводе дат
+            Toast.makeText(this, "Даты введены неверно не совпадают", Toast.LENGTH_LONG).show();
         }*/
 
-    RegisterUserController controller = new RegisterUserController();
-        controller.start(this, User);
+        editText = findViewById(R.id.edit_repeatPassword);
+        String recPassword = editText.getText().toString();
+        if (recPassword.equals(user.password)) {
+            RegisterUserController controller = new RegisterUserController();
+            controller.start(this, user);
+        } else {
+            Toast.makeText(this, "Пароли не совпадают", Toast.LENGTH_LONG).show();
+        }
 
+    }
 }
-
